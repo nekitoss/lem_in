0d7719484f537e241465de7dbdb728b1c9c360ec
Nick <nekit_p@ukr.net>
2017-08-14 21:18:07 +0300

    read data in list, print data, checkers of row type, start parsing data, some error with memalloc instead malloc

--- main.c
+++ main.c
@@ -20,32 +20,142 @@ ERROR: errormsg
 матрица смежности
 */
 
-typedef struct s_indata
-{
-	char		*str;
-	t_indata	*next;
-}				t_indata;
+#define RAW_D (ls->raw)
+#define ROOM (ls->rooms)
 
-typedef struct	s_room
+typedef struct 		s_indata
 {
-	char		*name;
-	size_t		num;
-	t_room		*next;
-}				t_room;
+	char			*str;
+	struct s_indata	*next;
+}					t_indata;
 
-typedef struct	s_lemin
+typedef struct		s_room
 {
-	int			ant_num;
-	t_room		*rooms;
-	t_indata	*raw;
-}				t_lemin;
+	char			*name;
+	size_t			num;
+	struct s_room	*next;
+}					t_room;
 
-void	read_input(t_lemin *ls)
+typedef struct		s_lemin
 {
-	
+	int				ant_num;
+	t_room			*rooms;
+	t_indata		*raw;
+	size_t			start_room;
+	size_t			end_room;
+}					t_lemin;
+
+void				my_error( char *str)//, t_lemin *ls)
+{
+	printf("ERROR:%s\n", str);
+	//freestructure
+	// ls = NULL;
+	exit (1);
 }
 
-int				main(int argc, char **argv)
+void				add_data(t_indata **head, char *str)
+{
+	t_indata *tmp;
+	tmp = *head;
+	if (tmp == NULL)
+	{
+		// *head = (t_indata *)ft_memalloc(sizeof(t_indata));
+		*head = (t_indata *)malloc(sizeof(t_indata));
+		(*head)->next = NULL;
+		(*head)->str = (str);
+	}
+	else
+	{
+		while(tmp->next)
+			tmp = tmp->next;
+		// tmp->next = (t_indata *)ft_memalloc(sizeof(t_indata));
+		tmp->next = (t_indata *)malloc(sizeof(t_indata));
+		tmp->next->str = (str);
+		tmp->next->next = NULL;
+	}
+}
+
+int					is_comment(char *str)
+{
+	if (str && *str == '#' && str[1] && str[1] != '#')
+		return (1);
+	return (0);
+}
+
+int					is_command(char *str)
+{
+	if (str && *str == '#' && str[1] && str[1] == '#')
+		return (1);
+	return (0);
+}
+
+int					is_good_name(char *str)
+{
+	if (str && *str != '\0' && *str != '#' && *str != 'L')
+		return (1);
+	return (0);
+}
+
+t_indata			*find_ant_num(t_indata *tmp, t_lemin *ls)
+{
+	while (tmp)
+	{
+		// printf("trying: %s\n", tmp->str);
+		if (!(is_comment(tmp->str)))
+		{
+			if (!(ft_is_number(tmp->str, 0, -1)))
+				my_error("First row is NOT a number!");
+			if ((ls->ant_num = ft_atoi(tmp->str)) < 1)
+				my_error("No ants!");
+			tmp = tmp->next;
+			return (tmp);
+		}
+		tmp = tmp->next;
+	}
+	my_error("no rows!");
+	return (NULL);
+}
+
+void				parse_data(t_lemin *ls)
+{
+	t_indata *tmp;
+
+	tmp = find_ant_num(RAW_D, ls);
+	while (tmp)
+	{
+		if (!(is_comment(tmp->str)))
+		{
+			
+			
+			// if (*start = *end)
+		}
+		tmp = tmp->next;
+	}
+}
+
+void				print_indata(t_lemin *ls)
+{
+	t_indata *tmp;
+
+	tmp = RAW_D;
+	while (tmp)
+	{
+		printf("%s\n", tmp->str);
+		tmp = tmp->next;
+	}
+}
+
+void				read_input(t_lemin *ls)
+{
+	char	*buf;
+
+	while(get_next_line(0, &buf) && *buf != '\0')
+		add_data(&RAW_D, buf);
+	print_indata(ls);
+	parse_data(ls);
+}
+
+int					main(void)
 {
 	t_lemin	*ls;
 
